d824d42d0f2991510ee140b6ba8d04ad50614cdd
AjayAchugatla <ajayachugatla17@gmail.com>
2025-04-01 22:16:20 +0530

    Implement authentication flow with login page, enhance customer fetching, and update metadata for dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import Table from '@/app/ui/customers/table';
+import { fetchCustomers } from '@/app/lib/data';
+
+export default async function Page() {
+    const customers = await fetchCustomers()
+    //@ts-ignore
+    return <Table customers={customers} />
 }
