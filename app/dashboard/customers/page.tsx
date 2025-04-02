2f9603c943809ab9fc6979871bb57d68daa14032
AjayAchugatla <ajayachugatla17@gmail.com>
2025-04-02 11:13:38 +0530

    Enhance customer page to support search functionality with query parameters and add loading skeleton

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,21 @@
 import Table from '@/app/ui/customers/table';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 
-export default async function Page() {
-    const customers = await fetchFilteredCustomers("")
-    return <Table customers={customers} />
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query)
+    return (
+        <div className="w-full">
+            <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
+                <Table customers={customers} />
+            </Suspense>
+        </div>
+    );
 }
